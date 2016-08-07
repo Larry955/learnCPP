0ea072f4fac38e5b7d9927551170694df2c3ec35
Larry King <1412857955@qq.com>
2016-08-07 21:43:15 +0800

    use_find
    a simple program to test the function "find",which is used to make us
    have a basic comprehension about generic algorithm

--- code/L10  Generic Algorithms/use_find/use_find/use_find.cpp
+++ code/L10  Generic Algorithms/use_find/use_find/use_find.cpp
@@ -17,5 +17,29 @@ int main()
 {
 	int ia[] = { 27, 210, 87, 14, 27, 83, 127 };
 	int value = 83;
-	int *result = find(begin(ia), end(ia), value);
+
+	//search starting from ia[0] up to but not including ia[6]
+	auto result = find(ia, ia + 6, value);	
+	cout << "The value " << (result == ia + 7 ? "is not present" : "is present") << endl;
+
+	//initial the vector with some values
+	vector<int>vec = { 27, 210, 87, 14, 27, 83, 127 };
+	int val = 42;	//value we will look for
+
+	//result2 will denote the element we want if it's vec
+	//or vec.cend() if not
+	auto result2 = find(vec.cbegin(), vec.cend(), val);
+	cout << "The value " << val
+		<< (result2 == vec.cend() ? " is not present" : "is present") << endl;
+
+	//now use find to look in a list of strings
+	list<string>lst = { "val1", "val2", "val3" };
+
+	string sval = "a value";		//value we will look for
+	//this call to find looks through string elements in a list
+	auto result3 = find(lst.begin(), lst.end(), sval);
+	cout << "The value " << sval
+		<< (result3 == lst.end() ? " is not present" : "is present") << endl;
+
+	getchar();
 }
