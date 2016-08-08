a60ee17e34739859844e3392a556e665dd1a8dbd
Larry King <1412857955@qq.com>
2016-08-08 21:37:48 +0800

    ostreamIter2
    istream_iterator,ostream_iterator

---
+++ code/L10  Generic Algorithms/ostreamIter2/ostreamIter2/ostreamItem2.cpp
@@ -0,0 +1,21 @@
+#include<iterator>
+using std::istream_iterator; using std::ostream_iterator;
+
+#include<vector>
+using std::vector;
+
+#include<iostream>
+using std::cin; using std::cout; using std::endl;
+
+int main()
+{
+	vector<int>vec;
+	istream_iterator<int>in_iter(cin), eof;		//read ints from cin
+	while (in_iter != eof)
+		vec.push_back(*in_iter++);
+	ostream_iterator<int>out_iter(cout, " ");
+	for (auto e : vec)
+		out_iter = e;		//the assignment writes this element to cout
+	cout << endl;
+	getchar();
+}
