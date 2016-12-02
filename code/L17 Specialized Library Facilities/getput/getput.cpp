2d46a97f713ffe405f248cca490a79cc5d901451
Larry King <1412857955@qq.com>
2016-12-02 11:22:58 +0800

    a example of using std::cin.get() &&std::cout.put()

---
+++ code/L17 Specialized Library Facilities/getput/getput.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using std::cin; using std::cout; using std::endl;
+
+int main()
+{
+	char ch;
+	while (cin.get(ch))
+	{
+		cout.put(ch);
+	}
+	cout << endl;
+	system("pause");
+}
